98beb199469dc0341c70664c321da3790f955dfb
Ozgur OZTURK <oozgurozturk@gmail.com>
2021-04-29 01:03:51 +0200

    last

--- src/main/java/com/oozgurozturk/restful/webapi/todoapp/HelloWorldController.java
+++ src/main/java/com/oozgurozturk/restful/webapi/todoapp/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World!!";
+		return "Hello World!";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
